5a0d11562e5274463494d045318cd6377f2e39a9
doudou <2814143768@qq.com>
2021-02-08 20:28:54 +0800

    fixed bugs

--- cdou/main.c
+++ cdou/main.c
@@ -2,14 +2,81 @@
 #include <stdlib.h>
 #include <memory.h>
 #include <string.h>
+#define int32 int
+#define int long
 
-int token;
+int token, tmp;
 char *src, *old_src;
 int poolsize;
 int line;
 
+/* 词法分析器 */
+enum {
+    Num = 128, Fun, Sys, Glo, Loc, Id, Char, Else, Enum, If, Int, Return, Sizeof, While, Assign, Cond, Lor, Lan, Or, Xor, And, Eq, Ne, Lt, Gt, Le, Ge, Shl, Shr, Add, Sub, Mul, Div, Mod, Inc, Dec, Brak
+};
+
+int token_val;
+int *current_id, *symbols;
+
+enum {
+    Token, Hash, Name, Type, Class, Value, BType, BClass, BValue, IdSize
+};
+
+struct identifier {
+    int token;
+    int hash;
+    char * name;
+    int class;
+    int type;
+    int value;
+    int Bclass;
+    int Btype;
+    int Bvalue;
+};
+
 void next() {
-    token = *src++;
+    printf("%c", *src);
+    char *last_pos;
+    int hash;
+
+    while (token = *src) {
+        printf("%d", src);
+        ++src;
+        if (token == '\n') {
+            ++line;
+        } else if (token == '#') {
+            while (*src != 0 && *src != '\n') {
+                src++;
+            }
+        } else if ((token >= 'a' && token <= 'z') || (token >='A' && token <= 'Z') || (token == '_')) {
+            printf("%d", last_pos);
+            last_pos = src-1;
+            printf("%d", last_pos);
+            hash = token;
+
+            while ((*src >= 'a' && *src <= 'z') || (*src >= 'A' && *src <= 'Z') || (*src >= '0' && *src <= '9') || (*src == '_')) {
+                hash = hash*147 + *src;
+                src++;
+            }
+
+            current_id = symbols;
+            while (current_id[Token]) {
+                if (current_id[Hash] == hash && !memcmp((char *)current_id[Name], last_pos, src-last_pos)) {
+                    token = current_id[Token];
+                    return;
+                }
+                current_id = current_id + IdSize;
+            }
+
+            current_id[Name] = (int)last_pos;
+            current_id[Hash] = hash;
+            token = current_id[Token] = Id;
+            return;
+
+        }
+            
+                
+    }
     return;
 }
 
@@ -18,6 +85,7 @@ void expression(int level) {
 }
 
 void program() {
+    printf("hello,world");
     next();
     while (token > 0) {
         printf("token is: %c\n", token);
@@ -31,11 +99,92 @@ char *data;
 
 int *pc, *bp, *sp, ax, cycle;
 
+enum { LEA, IMM, JMP, CALL, JZ, JNZ, ENT, ADJ, LEV, LI, LC, SI, SC, PUSH, OR, XOR, AND, EQ, NE, LT, GT, LE, GE, SHL, SHR, ADD, SUB, MUL, DIV, MOD, OPEN, READ, CLOS, PRTF, MALC, MSET, MCMP, EXIT };
+
 int eval() {
+    int op, *tmp;
+    while (1) {
+        op = *pc++;
+//        printf("%d\n", op);
+        if (op == IMM) {
+            ax = *pc++;
+        } else if (op == LC) {
+            ax = *(char *)ax;
+        } else if (op == LI) {
+            ax = *(int *)ax;
+        } else if (op == SC) {
+            *(char *)*sp++ = ax;
+        } else if (op == SI) {
+            *(int *)*sp++ = ax;
+        } else if (op == PUSH) {
+//            printf("%d\n", sp);
+            *--sp = ax;
+//            printf("%d\n", sp);
+        } else if (op == JMP) {
+            pc = (int *)*pc;
+        } else if (op == JZ) {
+            pc = ax ? pc+1 : (int *)*pc;
+        } else if (op == JNZ) {
+            pc = ax ? (int *)*pc : pc+1;
+        } else if (op == CALL) {
+            *--sp = (int)(pc+1);
+            pc = (int *)*pc;
+        } else if (op == ENT) {
+            *--sp = (int)bp;
+            bp = sp;
+            sp = sp - *pc++;
+        } else if (op == ADJ) {
+            sp = sp + *pc++;
+        } else if (op == LEV) {
+            sp = bp;
+            bp = (int *)*sp++;
+            pc = (int *)*sp++;
+        } else if (op == LEA) {
+            ax = (int)(bp + *pc++);
+        } else if (op == OR) ax = *sp++ | ax;
+        else if (op == XOR) ax = *sp++ ^ ax;
+        else if (op == AND) ax = *sp++ & ax;
+        else if (op == EQ) ax = *sp++ == ax;
+        else if (op == NE) ax = *sp++ != ax;
+        else if (op == LT) ax = *sp++ < ax;
+        else if (op == LE) ax = *sp++ <= ax;
+        else if (op == GT) ax = *sp++ > ax;
+        else if (op == GE) ax = *sp++ >= ax;
+        else if (op == SHL) ax = *sp++ << ax;
+        else if (op == SHR) ax = *sp++ >> ax;
+        else if (op == ADD) ax = *sp++ + ax;
+        else if (op == SUB) ax = *sp++ - ax;
+        else if (op == MUL) ax = *sp++ * ax;
+        else if (op == DIV) ax = *sp++ / ax;
+        else if (op == MOD) ax = *sp++ % ax;
+        else if (op == EXIT) {
+            printf("exit(%d)", *sp);
+            return *sp;
+        } else if (op == OPEN) {
+            ax = open((char *)sp[1], sp[0]);
+        } else if (op == CLOS) {
+            ax = close(*sp);
+        } else if (op == READ) {
+            ax = read(sp[2], (char *)sp[1], *sp);
+        } else if (op == PRTF) {
+            tmp = sp + pc[1];
+            ax = printf((char *)tmp[-1], tmp[-2], tmp[-3], tmp[-4], tmp[-5], tmp[-6]);
+        } else if (op == MALC) {
+            ax = (int)malloc(*sp);
+        } else if (op == MSET) {
+            ax = (int)memset((char *)sp[2], sp[1], *sp);
+        } else if (op == MCMP) {
+            ax = memcmp((char *)sp[2], (char *)sp[1], *sp);
+        } else {
+            printf("unknown instruction: %d]n", op);
+            return -1;
+        }
+    }
     return 0;
 }
 
 int main(int argc, char **argv) {
+    
     int i, fd;
 
     argc--;
@@ -74,10 +223,25 @@ int main(int argc, char **argv) {
     memset(text, 0, poolsize);
     memset(data, 0, poolsize);
     memset(stack, 0, poolsize); 
+    memset(symbols, 0, poolsize);
 
     bp = sp = (int *)((int)stack + poolsize);
     ax = 0;
 
-    program();
+    i = 0;
+    text[i++] = IMM;
+    text[i++] = 10;
+    text[i++] = PUSH;
+    text[i++] = IMM;
+    text[i++] = 20;
+    text[i++] = ADD;
+    text[i++] = PUSH;
+    text[i++] = EXIT;
+    pc = text;
+
+
+    printf("hello\n");
+    next();
+    //program();
     return eval();
 }
