995dcbc515635123bc85edf12d111b85f730649b
doudou <2814143768@qq.com>
2021-02-26 00:18:27 +0800

    fixed bugs

--- cdou/main.c
+++ cdou/main.c
@@ -247,7 +247,71 @@ void expression(int level) {
 }
 
 void statement() {
-    return;
+    int *a, *b;
+
+    if (token == If) {
+        match(If);
+        match('(');
+        expression(Assign);
+        printf("statement\n");
+        match(')');
+
+        *++text = JZ;
+        b = ++text;
+
+        statement();
+        if (token == Else) {
+            match(Else);
+
+            *b = (int)(text + 3);
+            *++text = JMP;
+            b = ++text;
+
+            statement();
+        }
+
+        *b = (int)(text + 1);
+    } else if (token == While) {
+        match(While);
+
+        a = text + 1;
+
+        match('(');
+        expression(Assign);
+        match(')');
+
+        *++text = JZ;
+        b = ++text;
+
+        statement();
+
+        *++text = JMP;
+        *++text = (int)a;
+        *b = (int)(text + 1);
+    } else if (token == Return) {
+        match(Return);
+
+        if (token != ';') {
+            expression(Assign);
+        }
+
+        match(';');
+
+        *++text = LEV;
+    } else if (token == '{') {
+        match('{');
+        
+        while (token != '}') {
+            statement();
+        }
+
+        match('}');
+    } else if (token == ';') {
+        match(';');
+    } else {
+        expression(Assign);
+        match(';');
+    }
 }
 
 
